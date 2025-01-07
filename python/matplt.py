93c87e19c884b61f634878985bd690c508e53a60
aleinsf <aleinsf99@gmail.com>
2025-01-06 23:03:26 -0300

    mas de mtl

--- python/matplt.py
+++ python/matplt.py
@@ -1,3 +1,9 @@
-import matplotlib as mtl 
+import matplotlib as mtl
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots()
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+plt.show()
+
 
 print(mtl.__version__)
