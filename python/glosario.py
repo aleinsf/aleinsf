14f196155948c5276293db71b51732506a8d457f
aleinsf <aleinsf99@gmail.com>
2025-01-07 23:06:02 -0300

    act glosario

--- python/glosario.py
+++ python/glosario.py
@@ -1,3 +1,5 @@
+import random 
+
 # datos simples
 "string"
 'string'
@@ -108,15 +110,15 @@ coincidencias = mayusc.count("a")
 
 # def
 
+num = random.randint(1,28)
+
 def crear_contraseña_random(num):
      chars = "abcdefghijklmnñopqrstuvwxyz"
      num_entero = str(num)
-     num = int(num_entero[0])
      c1 = num - 2
      c2 = num
      c3 = num - 5 
-     contraseña = f"{chars[c1]}{chars[c2]}{chars[c3]}{num*2}{num//12}"
+     contraseña = f"{chars[c1]}{chars[c2]}{chars[c3]}{num*2}{num**12}"
      print(contraseña)
 
-crear_contraseña_random(2)
-
+crear_contraseña_random(num)
