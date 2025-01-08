1c21270c418988cb9e0f62f0f376b793521dd6e8
aleinsf <aleinsf99@gmail.com>
2025-01-07 23:01:32 -0300

    matplt progreso

--- python/matplt.py
+++ python/matplt.py
@@ -1,9 +1,23 @@
 import matplotlib as mtl
 import matplotlib.pyplot as plt
 
-fig, ax = plt.subplots()
-ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+# fig, ax = plt.subplots()
+# ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+# plt.show()
+
+############ pyplot 
+
+plt.plot([1, 2, 3, 4], [1, 4, 2, 3])
+plt.xlabel('x')
+plt.ylabel('y')
+plt.title('Simple plot')
 plt.show()
 
+############# OOP
 
-print(mtl.__version__)
+fig, ax = plt.subplots()
+ax.plot([1, 2, 3, 4], [1, 4, 2, 3])
+ax.set_xlabel('x')  
+ax.set_ylabel('y')  
+ax.set_title('Simple plot OOP')
+plt.show()
