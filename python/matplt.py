2a3a953fb1ea079a4e14aa7483e4211a7cfb521a
aleinsf <aleinsf99@gmail.com>
2025-01-08 23:51:21 -0300

    plt act

--- python/matplt.py
+++ python/matplt.py
@@ -21,3 +21,31 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')  
 ax.set_title('Simple plot OOP')
 plt.show()
+
+############# 
+import numpy as np
+
+x = np.linspace(0, 2, 100)
+
+plt.figure(figsize=(100,50), layout="constrained")
+plt.plot(x, x, label='linear')
+plt.plot(x, x**2, label='quadratic')
+plt.plot(x, x**3, label='cubic')
+plt.xlabel('x label')
+plt.ylabel('y label')   
+plt.title("Simple plot")
+plt.legend()
+plt.show()
+
+x = np.linspace(0, 2, 100)
+
+fig, ax = plt.subplots()
+ax.plot(x, x, label='linear')   
+ax.plot(x, x**2, label='quadratic') 
+ax.plot(x, x**3, label='cubic') 
+ax.set_xlabel('x label')
+ax.set_ylabel('y label')
+ax.set_title("Simple plot OOP")
+ax.legend()
+plt.show()
+
